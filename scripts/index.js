ea65b0e378d3cd839a96e9fc8cdbae28d97f0825
dianaki <diana.kuramshina21@gmail.com>
2020-12-22 13:43:43 +0300

    complete videoPlayer

--- scripts/index.js
+++ scripts/index.js
@@ -10,10 +10,14 @@ const deactivationPlayer = () => {
   temp.style.display = "none";
   playerBtn.forEach(item => item.classList.remove("active"));
   playerBlock.forEach(item => item.classList.remove("active"));
-};
+}; // скрытие неактивного плеера
 
 playerBtn.forEach((btn, index) => btn.addEventListener("click", () => {
   deactivationPlayer();
   btn.classList.add("active");
   playerBlock[index].classList.add('active');
-}));
+})); // отображение выбранного плеера при нажатии на соответсвующую ему кнопку
+
+videoPlayerInit();
+radioPlayerInit();
+musicPlayerInit();
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,62 @@
 export const videoPlayerInit = () => {
-  console.log("video init");
+
+  const videoPlayer = document.querySelector('.video-player');
+  const videoButtonPlay = document.querySelector('.video-button__play');
+  const videoButtonStop = document.querySelector('.video-button__stop');
+  const videoProgress = document.querySelector('.video-progress');
+  const videoTimePassed = document.querySelector('.video-time__passed');
+  const videoTimeTotal = document.querySelector('.video-time__total');
+
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove('fa-pause');
+      videoButtonPlay.classList.add('fa-play');
+    } else {
+      videoButtonPlay.classList.add('fa-pause');
+      videoButtonPlay.classList.remove('fa-play');
+    }
+  } // изменение иконки кнопки play на pause и наоборот
+
+  const togglePlay = () => {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    }
+  }; // изменение состояния видео 
+
+  const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+  } // остановка проигрывания видео и сброс его времени
+
+  const addZero = n => n < 10 ? '0' + n : n; // добавление нуля для корректного отображения проигранного времени видео
+
+  videoPlayer.addEventListener('click', togglePlay);
+  videoPlayer.addEventListener('play', toggleIcon);
+  videoPlayer.addEventListener('pause', toggleIcon);
+  videoButtonPlay.addEventListener('click', togglePlay);
+  videoButtonStop.addEventListener('click', stopPlay);
+
+  videoPlayer.addEventListener('timeupdate', () => {
+    const currentTime = videoPlayer.currentTime;
+    const duration = videoPlayer.duration;
+
+    videoProgress.value = (currentTime / duration) * 100; // определение положения бегунка 
+
+    let minutePassed = Math.floor(currentTime / 60); // определение проигранных минут
+    let secondsPassed = Math.floor(currentTime % 60); // определеник проигранных секунд
+
+    let minuteTotal = Math.floor(duration / 60); // определение минут общей продолжительности видео 
+    let secondsTotal = Math.floor(duration % 60); // определение секунд общей продолжительности видео
+
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`; // отображение проигранного времени видео
+    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`; // отображение общей продолжительности видео
+  });
+
+  videoProgress.addEventListener('change', () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+    videoPlayer.currentTime = (value * duration) / 100;
+  }) // переключение видео при изменении положения бегунка
 }
